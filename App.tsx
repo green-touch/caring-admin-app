b00e3f5f75851cfa6d38774aa72cfd2447d63a2c
jpark0506 <peterp1@inha.edu>
2025-04-11 21:02:05 +0900

    [CARE-66] feat : 절대경로 설정 완료

--- App.tsx
+++ App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { RootStackParamList, MainTabParamList } from './src/navigation/types';
 
-import SosListScreen from './src/\bscreens/SosListScreen';
-import ElderListScreen from './src/\bscreens/ElderListScreen';
-import MainScreen from './src/\bscreens/MainScreen';
-import LoginScreen from './src/\bscreens/LoginScreen';
 
+import { RootStackParamList, MainTabParamList } from '@_navigation/types';
+
+import MainScreen from '@_pages/MainScreen';
+import LoginScreen from '@_pages/LoginScreen';
+import SosListScreen from '@_pages/SosListScreen';
+import ElderListScreen from '@_pages/ElderListScreen';
+import Demo from '@_pages/Demo';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator<MainTabParamList>();
@@ -26,6 +28,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Demo" component={Demo} />
         <Stack.Screen name="Main" component={MainTabs} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/pages/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const LoginScreen = () => {
+    return (
+        <View className="flex-1 h-full justify-center items-center bg-white">
+            <Text className="text-xl text-black">LoginScreen</Text>
+        </View>
+    );
+};
+
+export default LoginScreen;
