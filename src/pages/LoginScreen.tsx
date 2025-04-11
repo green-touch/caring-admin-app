b704c52e728198b92e112bf616381ad888447371
jpark0506 <peterp1@inha.edu>
2025-04-11 21:14:30 +0900

    [CARE-66] feat : SVG 세팅 완료

--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -1,3 +1,4 @@
+import Icon from '@_components/SvgIcon';
 import React from 'react';
 import { Text, View } from 'react-native';
 
@@ -5,6 +6,7 @@ const LoginScreen = () => {
     return (
         <View className="flex-1 h-full justify-center items-center bg-white">
             <Text className="text-xl text-black">LoginScreen</Text>
+            <Icon name="Refresh" size={24} color="black" />
         </View>
     );
 };
