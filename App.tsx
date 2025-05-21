e74840893aad1e22bfe48aebdbda72a3a54689cb
unknown <admin@skmin-test.Kanavi-Auto.com>
2025-05-21 11:37:18 +0900

    [CARE-87] feat:  app.tsx  경로 추가 및 네임 변경

--- App.tsx
+++ App.tsx
@@ -10,8 +10,19 @@ import Demo from '@_pages/Demo';
 import Sidebar from '@_pages/Sidebar';
 import ResetPassword from '@_pages/ResetPassword';
 import HelpResult from '@_pages/HelpResult';
-
+import SosDetailScreen from '@_pages/SosDetail';
+import SosEmergencyDetailScreen from '@_pages/SosEmergencyDetail';
+import UserDetail from '@_pages/ElderDetail';
+import ElderListScreen from '@_pages/ElderListScreen';
 const Stack = createNativeStackNavigator<RootStackParamList>();
+const ListStack = createNativeStackNavigator();
+
+const ElderListStackNavigator = () => (
+  <ListStack.Navigator screenOptions={{ headerShown: false }}>
+    <ListStack.Screen name="ElderList" component={ElderListScreen} />
+    <ListStack.Screen name="User" component={UserDetail} />
+  </ListStack.Navigator>
+);
 
 export default function App() {
   return (
@@ -23,10 +34,12 @@ export default function App() {
         }}
         initialRouteName="Login"
       >
+
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Demo" component={Demo} />
         <Stack.Screen name="Main" component={MainScreen} />
-        <Stack.Screen name="Sidebar" component={Sidebar} />
+        <Stack.Screen name="User" component={UserDetail} />
+        <Stack.Screen name="Elder" component={ElderListStackNavigator}/>
         <Stack.Screen name="ResetPassword" component={ResetPassword} />
         <Stack.Screen name="HelpResult" component={HelpResult} />
       </Stack.Navigator>
